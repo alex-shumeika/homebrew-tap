3eeb3b35a6cdddb3218a20022d4bb9faa8183401
Alex Shumeika <alex.shumeika@moonpig.com>
2026-01-03 01:24:59 +0000

    Update quickrun to use tar link

--- Formula/quickrun.rb
+++ Formula/quickrun.rb
@@ -1,7 +1,8 @@
 class Quickrun < Formula
   desc "Shortcut for frequently used commands"
   homepage "https://github.com/alex-shumeika/quickrun"
-  url "https://github.com/alex-shumeika/quickrun.git", tag: "0.1.1"
+  url "https://github.com/alex-shumeika/quickrun/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "78d4ebf8d20a4d65f79f10f171c742873c0153089eb611623bdc2975c09b7271"
   version "0.1.1"
 
   depends_on "xcode": [:build]
