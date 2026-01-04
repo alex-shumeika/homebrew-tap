6b9626b96dd7475bc5c03a2d1741c3a50f58952e
Alex Shumeika <alex.shumeika@moonpig.com>
2026-01-04 19:11:34 +0000

    Update to use pre-built instead of source code

--- Formula/quickrun.rb
+++ Formula/quickrun.rb
@@ -1,14 +1,12 @@
 class Quickrun < Formula
   desc "Shortcut for frequently used commands"
   homepage "https://github.com/alex-shumeika/quickrun"
-  url "https://github.com/alex-shumeika/quickrun/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "78d4ebf8d20a4d65f79f10f171c742873c0153089eb611623bdc2975c09b7271"
+  url "https://github.com/alex-shumeika/quickrun/releases/download/0.1.1/quickrun-0.1.1-macos-arm64.tar.gz"
+  sha256 "e8ea2b7b11111917ffb34471c94ac6ffad7ab581011cd603d1091f82ef476592"
   version "0.1.1"
 
-  depends_on "xcode": [:build]
-
   def install
-    system "make", "install", "prefix=#{prefix}"
+    bin.install "quickrun"
   end
 
   test do
